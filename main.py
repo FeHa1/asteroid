925ab70c9eb0c3542b9bfcfaf50749e2b0fea2cc
Federico Hayes <fedehayes2001@gmail.com>
2025-01-22 00:31:21 -0300

    logic

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black = (0, 0, 0)
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while (True): 
         for event in pygame.event.get():
             if event.type == pygame.QUIT: #for closing the window
                 return
         screen.fill(black)
         pygame.display.flip()
-
-    print("Starting asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
